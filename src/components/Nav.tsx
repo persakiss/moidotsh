ef5af1deb8f2aee0b2cde8ef7d9f4505b7706655
Arman <persakiss@gmail.com>
2023-09-20 15:39:37 -0400

    doing something with the navigation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,30 +1,90 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useMemo } from "react";
+import { Minus, GitCommit } from "react-feather";
+import { useState } from "react";
 
 type Props = {};
 
-function Nav({ }: Props) {
+function Nav({}: Props) {
   const router = useRouter();
 
+  const links = [["/"], ["/projects"], ["/skills"], ["/contact"]];
+
+
+
+
+  const iconMaker = (path: string, index: number) => {
+    return (
+        <li key={index}>
+          {router.pathname === path ? (
+            <GitCommit className="inline" />
+          ) : (
+            <Minus className="inline" />
+          )}
+        </li>
+    );
+  };
+
+  const iconsToJsx = (list: string[][]): JSX.Element[] => {
+    return list.map((route, index) => {
+      return iconMaker(route[0], index);
+    });
+  };
 
   // Abstracted this for some reason
-  const linkMaker = (path: string, name: string, style: string, offStyle = '') => {
+  const linkMaker = (
+    path: string,
+    index: number,
+    firstCapital: boolean,
+    style: string = "",
+    offStyle: string = ""
+  ) => {
+
+    let textOut;
+
+    if (path === "/") {
+      textOut = "home";
+    } else if (firstCapital) {
+      textOut = path[1].toUpperCase() + path.substring(2);
+    } else {
+      textOut = path.substring(1);
+    }
+
     return (
-      <li className={router.pathname === path ? style : offStyle}>
-        <Link href={path} scroll={false}>{name}</Link>
-      </li>
-    )
-  }
+        <li className={router.pathname === path ? style : offStyle} key={index}>
+          <Link href={path} scroll={false} onClick={() => {}}> 
+            {textOut}
+          </Link>
+        </li>
+    );
+  };
+
+  const linksToJsx = (list: string[][]): JSX.Element[] => {
+    return list.map((route, index) => {
+      return linkMaker(route[0], index, false);
+    });
+  };
 
   return (
-    <nav className="w-full flex mt-10">
-      <ul className="flex-auto">
-       {linkMaker('/',          'Home', 'underline')}
-       {linkMaker('/projects',  'Projects', 'underline')}
-       {linkMaker('/skills',    'Skills', 'underline')}
-       {linkMaker('/contact',   'Contact', 'underline')}
-      </ul>
+    <nav className="w-full flex flex-col mt-10 ">
+      <div className="mx-auto">
+        <ul className="flex justify-between relative">
+          {iconsToJsx(links)}
+          <style jsx>{`
+            ul::before {
+              content: "";
+              position: absolute;
+              top: 50%;
+              left: 0;
+              right: 0;
+              height: 2px;
+              background-color: black;
+            }
+          `}</style>
+        </ul>
+        <ul className="flex justify-between">{linksToJsx(links)}</ul>
+      </div>
     </nav>
   );
 }

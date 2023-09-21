d600b451f4eba007849dbedbdd9b2e5771373031
Arman <persakiss@gmail.com>
2023-09-21 11:43:05 -0400

    pushing for sync, fix file structure, it was self-referential

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,86 +1,73 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useMemo } from "react";
-import { Minus, GitCommit } from "react-feather";
+import { Folder, File } from "react-feather";
 import { useState } from "react";
+
 
 type Props = {};
 
-function Nav({}: Props) {
+function Nav({ }: Props) {
   const router = useRouter();
 
-  const links = [["/"], ["/projects"], ["/skills"], ["/contact"]];
+  const [hidden, setHidden] = useState(false)
 
-  const iconMaker = (path: string, index: number) => {
-    return (
-      <li key={index} className="">
-        {router.pathname === path ? "|" : ""}
-      </li>
-    );
-  };
 
-  const iconsToJsx = (list: string[][]): JSX.Element[] => {
-    return list.map((route, index) => {
-      return iconMaker(route[0], index);
-    });
-  };
+  const rootFolder = {
+    name: "/home/arman",
+    type: "folder",
+    parent: null, 
+    children: [
+      {
+        name: "projects",
+        type: "folder",
+        path: "/projects",
+        parent: rootFolder,
+        children: [],
+      },
+      {
+        name: "skills",
+        type: "folder",
+        path: "/skills",
+      },
+      {
+        name: "contact",
+        type: "folder",
+        path: "/contact",
+      },
+      {
+        name: "index.tsx",
+        type: "file",
+        path: "/"
+      }
+    ]
+  }
 
-  // Abstracted this for some reason
-  const linkMaker = (
-    path: string,
-    index: number,
-    firstCapital: boolean,
-    style: string = "",
-    offStyle: string = ""
-  ) => {
-    let textOut;
-    let onStyle;
 
-    if (path === "/") {
-      onStyle = "";
-      textOut = "home";
-    } else if (firstCapital) {
-      textOut = path[1].toUpperCase() + path.substring(2);
-    } else {
-      textOut = path.substring(1);
-    }
+  const textMaker = (inputPath: typeof router.pathname) => {
+    const preface = "/home/arman"
+    return preface + inputPath;
+  }
 
-    return (
-      <li
-        className={router.pathname === path ? style + onStyle : offStyle}
-        key={index}
-      >
-        <Link href={path} scroll={false} onClick={() => {}}>
-          {textOut}
-        </Link>
-      </li>
-    );
-  };
-
-  const linksToJsx = (list: string[][]): JSX.Element[] => {
-    return list.map((route, index) => {
-      return linkMaker(route[0], index, false);
-    });
-  };
 
   return (
     <nav className="w-full flex flex-col">
-      <div className="mx-auto ">
-        <ul className="flex justify-between relative">
-          {iconsToJsx(links)}
-          <style jsx>{`
-            ul::before {
-              content: "";
-              position: absolute;
-              top: 50%;
-              left: 0;
-              right: 0;
-              height: 2px;
-              background-color: black;
-            }
-          `}</style>
+      <div className="absolute mt-2 h-[5vh] w-[23rem] flex self-center bg-white font rounded-t">
+        <p className="self-center flex-auto">{textMaker(router.pathname)}</p> <p className="mr-3 mt-1" onClick={() => setHidden(!hidden)}>x</p></div>
+      <div className="absolute mt-[5vh] h-[7rem] w-[23rem] flex self-center bg-white rounded-b opacity-30" style={{ display: hidden ? "none" : "" }}> </div>
+      <div className="mx-auto">
+        <ul className="grid grid-cols-4 mt-14" style={{ display: hidden ? "none" : "" }}>
+          <li className="col-start-1"><Folder /></li>
+          <li className="col-start-2"><Folder /></li>
+          <li className="col-start-3"><Folder /></li>
+          <li className="col-start-4"><File /></li>
         </ul>
-        <ul className="flex justify-between -mt-6">{linksToJsx(links)}</ul>
+        <ul className="grid grid-cols-4 -mt-6" style={{ display: hidden ? "none" : "" }}>
+          <li className="col-start-1">Projects</li>
+          <li className="col-start-2">Skills</li>
+          <li className="col-start-3">Contact</li>
+          <li className="col-start-4">index.tsx</li>
+        </ul>
       </div>
     </nav>
   );
--- src/components/TerminalContainer.tsx
+++ src/components/TerminalContainer.tsx
@@ -1,27 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {};
 
 function TerminalContainer({}: Props) {
+
+const [hidden, setHidden] = useState(false);
+
   return (
     <>
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -mr-10 z-[100] bg-white rounded opacity-30 h-[50vh] w-3/5"></div>
-      <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -mr-10 z-[100] bg-white rounded-t opacity-100 h-[5vh] w-3/5 ">
-        <div className="grid grid-cols-11 h-full">
-          <div className="col-start-2 lg:col-start-2 my-auto whitespace-nowrap">st ~</div>
-          <div className="col-start-11 my-auto whitespace-nowrap">_   . x</div>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-8 ml-9 z-[1000] bg-white rounded opacity-30 h-[50vh] w-2/3" style={{display: hidden ? "none" : ""}}></div>
+      <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-8 ml-9 z-[1000] bg-white rounded-t opacity-100 h-[5vh] w-2/3 ">
+        <div className="flex justify-between h-full">
+          <div className="my-auto whitespace-nowrap pl-3 hover:cursor-default">st ~</div>
+          <div className="my-auto whitespace-nowrap pr-3 hover:cursor-default"><p onClick={() => {
+            setHidden(!hidden);
+            console.log("clicked")
+            console.log(hidden)
+          }}>x</p></div>
         </div>
       </div>
 
       <div className="absolute mt-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -mr-10 z-[100] rounded h-[50vh] w-2/7">
-        <h2>Your Title Here</h2>
-        <img src="/path/to/your/image.jpg" alt="Description" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          labore omnis, vel aliquam aperiam mollitia corporis non corrupti,
-          consectetur suscipit aut repudiandae? Inventore, necessitatibus
-          aperiam dignissimos quod eum libero nam.
-        </p>
       </div>
     </>
   );

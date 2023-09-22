264b17a45abbf50a3173e26cf9e19fc4023333dc
Arman <persakiss@gmail.com>
2023-09-21 20:17:30 -0400

    vh-to-rem fix issue #19, also brought terminal outside of / index so it travels

--- src/components/AddressBar.tsx
+++ src/components/AddressBar.tsx
@@ -1,6 +1,7 @@
 // AddressBar.tsx
 import React from 'react';
 import { useRouter } from "next/router";
+import { X } from 'react-feather';
 
 type AddressBarProps = {
     hidden: boolean;
@@ -17,14 +18,14 @@ const AddressBar = ({ hidden, toggleHidden }: AddressBarProps) => {
 
     return (
         <>
-            <div className="absolute mt-2 h-[5vh] w-[20rem] flex self-center bg-white font rounded-t address-bar">
+            <div className="absolute mt-2 h-[2rem] w-[20rem] flex self-center bg-white font rounded-t address-bar">
                 <p className="self-center flex-auto">{textMaker(router.pathname)}</p>{" "}
-                <p className="mr-3 mt-1" onClick={toggleHidden}>
-                    x
+                <p className="mr-3 my-auto " onClick={toggleHidden}>
+                    <X size={"1rem"} />
                 </p>
             </div>
             <div
-                className="absolute mt-[5vh] h-[7rem] w-[20rem] flex self-center bg-white rounded-b opacity-30"
+                className="absolute mt-[2rem] h-[7rem] w-[20rem] flex self-center bg-white rounded-b opacity-30"
                 style={{ display: hidden ? "none" : "" }}
             >
                 {" "}

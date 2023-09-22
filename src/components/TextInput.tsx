1d05bac0416ba4f98fc58664ac577a4a9cad7c43
Arman <persakiss@gmail.com>
2023-09-21 23:20:20 -0400

    hope to fix errors, textinput had wrong props

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,51 +1,55 @@
 import React, { useState, ChangeEvent, KeyboardEvent } from "react";
+import { useRouter } from "next/router";
 
 type TextInputProps = {
   changeLine: (count: number) => void;
   initialValue: string;
-  handleCommandExecution: (commandInput: string) => void;
+  handleCommandExecution: (commandInput: string, router: ReturnType<typeof useRouter>) => void;
   afterEnter?: () => void;
+  router: ReturnType<typeof useRouter>;
 };
 
 const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
-  ({ changeLine, initialValue, handleCommandExecution, afterEnter }, ref) => {
-  const [width, setWidth] = useState(0);
-  const [count, setCount] = useState(0);
+  ({ router, changeLine, initialValue, handleCommandExecution, afterEnter }, ref) => {
+    const [width, setWidth] = useState(0);
+    const [count, setCount] = useState(0);
 
-  const changeHandler = (evt: ChangeEvent<HTMLInputElement>) => {
-    setWidth(evt.target.value.length);
-  };
+    const changeHandler = (evt: ChangeEvent<HTMLInputElement>) => {
+      setWidth(evt.target.value.length);
+    };
 
-const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
-  if (e.key === "Enter") {
-    handleCommandExecution(e.currentTarget.value);
-    setCount((prevCount) => prevCount + 1);
-    changeLine(count);
-    if (afterEnter) {
-      afterEnter();
-    }
-  }
-};
+    const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter") {
+        handleCommandExecution(e.currentTarget.value, router); // Use the router prop directly
+        setCount((prevCount) => prevCount + 1);
+        changeLine(count);
+        if (afterEnter) {
+          afterEnter();
+        }
+      }
+    };
 
 
-  return (
-    <div className={`s${count}`}>
-      <input
-        ref={ref}
-        type="text"
-        style={{ width: `${width}ch` }}
-        autoFocus
-        onChange={changeHandler}
-        id="console"
-        spellCheck="false"
-        autoComplete="off"
-        name="hidden"
-        onKeyPress={handleKeyPress}
-        className="bg-transparent border-none outline-none caret-transparent font-fira-code"
-      />
-    </div>
-  );
-});
+
+
+    return (
+      <div className={`s${count}`}>
+        <input
+          ref={ref}
+          type="text"
+          style={{ width: `${width}ch` }}
+          autoFocus
+          onChange={changeHandler}
+          id="console"
+          spellCheck="false"
+          autoComplete="off"
+          name="hidden"
+          onKeyPress={handleKeyPress}
+          className="bg-transparent border-none outline-none caret-transparent font-fira-code"
+        />
+      </div>
+    );
+  });
 
 TextInput.displayName = "TextInput";
 

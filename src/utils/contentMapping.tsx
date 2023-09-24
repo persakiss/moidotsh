5d6241df35c16a2c3ce287ddba048aff84e16a61
Arman <persakiss@gmail.com>
2023-09-24 17:26:16 -0400

    resolves #37

--- src/utils/contentMapping.tsx
+++ src/utils/contentMapping.tsx
@@ -1,6 +1,6 @@
 // ../utils/contentMapping.tsx
 const contentMapping: { [key: string]: JSX.Element } = {
-  "/index.tsx": <h2>Home Page</h2>,
+  "/index.tsx": <h2>Home Page</h2>, // fix this later lol
   "/contact/index.tsx": <h2>Contact Page</h2>,
   "/projects/index.tsx": <h2>Projects</h2>,
   "/skills/index.tsx": <h2>Skills</h2>

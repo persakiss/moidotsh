388ec163a2a45add0f7a62f6103bec5977eb3322
Arman <persakiss@gmail.com>
2023-10-05 11:48:04 -0400

    fixed skills

--- src/pages/skills/index.tsx
+++ src/pages/skills/index.tsx
@@ -4,6 +4,8 @@ import PageTransition from "@/components/PageTransition";
 import { forwardRef } from "react";
 import MiddleBot from "@/components/MiddleBot";
 import Bot from "@/components/Bot";
+import FurthestLayer from "@/components/FurthestLayer";
+import MiddleTop from "@/components/MiddleTop";
 
 type IndexPageProps = {};
 type IndexPageRef = React.ForwardedRef<HTMLDivElement>;
@@ -13,6 +15,8 @@ function IndexPage(props: IndexPageProps, ref: IndexPageRef) {
   return (
     <PageTransition ref={ref}>
       <div className="page2 h-screen overflow-x-hidden overflow-y-hidden">
+        <FurthestLayer />
+        <MiddleTop />
         <MiddleBot />
         <Bot />
       </div>
